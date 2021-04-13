0f3fe27484ce1ab25ad7212d649e78a62a17b4fd
huaxiaofeng <huaxiaofeng@wshifu.com>
2021-04-13 10:44:06 +0800

    project init

---
+++ my-ios-app/my-ios-app/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  my-ios-app
+//
+//  Created by mac on 2021/4/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ my-ios-app/my-ios-app/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  my-ios-app
+//
+//  Created by mac on 2021/4/13.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
